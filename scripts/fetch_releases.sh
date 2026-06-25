480ca4c85a1f4ff9314a547ad51352911e1a3097
Andrew Annex <2126916+AndrewAnnex@users.noreply.github.com>
2026-06-24 19:10:35 -0700

    added linux x86

--- scripts/fetch_releases.sh
+++ scripts/fetch_releases.sh
@@ -8,6 +8,7 @@ declare -A platforms
 platforms[mac_x64]='MacIntel_OSX_AppleC_64bit/packages/cspice.tar.Z'
 platforms[mac_arm]='MacM1_OSX_clang_64bit/packages/cspice.tar.Z'
 platforms[lin_x64]='PC_Linux_GCC_64bit/packages/cspice.tar.Z'
+platforms[lin_x86]='PC_Linux_GCC_32bit/packages/cspice.tar.Z'
 platforms[cyg_x64]='PC_Cygwin_GCC_64bit/packages/cspice.tar.gz'
 platforms[win_x64]='PC_Windows_VisualC_64bit/packages/cspice.zip'
 
